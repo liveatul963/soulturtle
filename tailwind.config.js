398a352420317eec27b374f9518cf22c483e1fe4
liveatulai <liveatulai@gmail.com>
2025-06-24 20:31:35 +0530

    Updated tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'serif': ['DM Serif Display', 'serif'],
+      },
+      animation: {
+        'cursor-blink': 'cursor-blink 1s steps(2) infinite',
+      },
+      keyframes: {
+        'cursor-blink': {
+          '0%, 50%': { opacity: '1' },
+          '51%, 100%': { opacity: '0' },
+        }
+      }
+    },
+  },
+  plugins: [],
+};
